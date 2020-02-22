9f20bcce1c42cc18a0a074884caec114ea6c25dc
Sergkon99 <sergkon99@yandex.ru>
2020-02-22 09:20:14 +0300

    fix

--- 1/src/on2d.py
+++ 1/src/on2d.py
@@ -7,12 +7,42 @@ class LineNotExists(Exception):
 
 class Point():
     # точка с координатами (x, y)
-    def __init__(self, *, x, y):
+    def __init__(self, *, x=0, y=0):
         self.x: float = x
         self.y: float = y
 
     def __str__(self) -> str:
         return f'({self.x},{self.y})'
+
+
+
+class Vector():
+    # вектор из точки a в точку b
+    def __init__(self, a: Point=Point(), b: Point=Point()):
+        self.a = a
+        self.b = b
+        # координаты вектора
+        self.x = b.x - a.x
+        self.y = b.y - a.y
+
+    def from_coords(self, *, x, y):
+        self.x = x
+        self.y = y
+        return self
+
+    def __str__(self):
+        return f'({self.x};{self.y})'
+
+    def len(self):
+        return self.dot(self)**(1/2)
+
+    def dot(self, v: 'Vector'):
+        # скалярное произведение
+        return self.x * v.x + self.y * v.y
+
+    def cross(self, v: 'Vector'):
+        # векторное произведение
+        return self.x * v.y - self.y * v.x
 
 
 class Position(Enum):
@@ -30,7 +60,7 @@ class Line():
         self.b = b
         self.c = c
         # вектор нормали
-        # self.n = Vector(a, b)
+        self.n = Vector().from_coords(x=a, y=b)
 
     def __str__(self) -> str:
         return f'{self.a}x + {self.b}y + {self.c} = 0'
@@ -44,27 +74,3 @@ class Line():
             abs(self.a*line.c - self.c*line.a) < eps):
             return Position.match
         return Position.parallel
-
-
-class Vector():
-    # вектор из точки a в точку b
-    def __init__(self, a: Point, b: Point):
-        self.a = a
-        self.b = b
-        # координаты вектора
-        self.x = b.x - a.x
-        self.y = b.y - a.y
-
-    def __str__(self):
-        return f'({self.x};{self.y})'
-
-    def len(self):
-        return self.dot(self)**(1/2)
-
-    def dot(self, v: 'Vector'):
-        # скалярное произведение
-        return self.x * v.x + self.y * v.y
-
-    def cross(self, v: 'Vector'):
-        # векторное произведение
-        return self.x * v.y - self.y * v.x
