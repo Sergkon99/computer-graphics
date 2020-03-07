c16fdc96ab3d83faadcdaa469acfbb528b23fe23
Sergkon99 <37593436+Sergkon99@users.noreply.github.com>
2020-03-07 11:03:55 +0300

    comments

--- 2/utils.py
+++ 2/utils.py
@@ -59,9 +59,9 @@ def to_point(v: Vector):
 
 def move_on(p: Point, x: float, y: float):
     """
-    @breif Смещение на (x, y) в декартовых координатах
-    @param x, y смещение
-    @return точка в декартовых координатах
+    Смещение на (x, y) в декартовых координатах
+    :param: x, y смещение
+    :return: точка в декартовых координатах
     """
     move = Matrix([
         [1, 0, x],
@@ -73,13 +73,13 @@ def move_on(p: Point, x: float, y: float):
 
 def reflection(p: Point, x: bool=False, y:bool=False):
     """
-    @breif Выполнит отражение точки относитьльно осей Ox, Oy или прямой X=Y
-    @param p: Точка
-    @param x, y: Показывает как производит отображение: 
+    Выполнит отражение точки относитьльно осей Ox, Oy или прямой X=Y
+    :param: p Точка
+    :param: x, : Показывает как производит отображение: 
             x==True and x==False - относительно Ox
             x==False and x==True - относительно Oy
             x==True and x==True - относительно X=Y
-    @return Точка-результат
+    :return: Точка-результат
     """
     ref_x = Matrix([
         [1, 0, 0],
@@ -107,9 +107,9 @@ def reflection(p: Point, x: bool=False, y:bool=False):
 
 def scale(p: Point, x: float=1, y: float=1):
     """
-    @breif Мастабирование по осям
-    @param x, y: Коэффиценты масшиабирования по осям соответсвенно
-    @return тока-результат
+    Масштабирование по осям
+    :param: x, y Коэффиценты масшnабирования по осям соответсвенно
+    :return: тока-результат
     """
     scale = Matrix([
         [x, 0, 0],
@@ -121,10 +121,10 @@ def scale(p: Point, x: float=1, y: float=1):
 
 def rotate(p: Point, angle: float):
     """
-    @breif Метод для поворота точки на угол
-    @param p: точка
-    @param angle: угол
-    @return Точка-результат
+    Метод для поворота точки на угол
+    :param: p точка
+    :param: angle угол
+    :return: Точка-результат
     """
     angle = angle*pi/180
     rotate = Matrix([
@@ -137,11 +137,11 @@ def rotate(p: Point, angle: float):
 
 def rotate_from(p: Point, angle: float, x: Point):
     """
-    @breif Метод для поворота точки на угол относитьльно другой точки
-    @param p: точка
-    @param angle: угол
-    @param x: точка отсчета
-    @return Точка-результат
+    Метод для поворота точки на угол относитьльно другой точки
+    :param: p точка
+    :param: angle угол
+    :param: x точка отсчета
+    :return: Точка-результат
     """
     angle = angle*pi/180
     move_to = Matrix([
